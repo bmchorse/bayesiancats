0ae4d19046107a6386c561fef7ff26fb96cb4795
bmchorse <bmchorse@gmail.com>
2017-06-21 15:57:38 -0400

    Refine cat list
    Delete more obscure breeds, add in some wild cats for variety.

--- bot.py
+++ bot.py
@@ -10,6 +10,27 @@ auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
 auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)  
 api = tweepy.API(auth)  
 
+cats = animals.cats['cats']
+
+# Remove some of the less recognizable cat breeds
+dropcats = ['Aegean', 'Asian', 'Australian Mist', 'Bambino', 
+'Bombay', 'Burmilla', 'California Spangled', 'Chantilly-Tiffany', 
+'Chartreux', 'Chausie', 'Cheetoh', 'Cymric', 'Cyprus', 'Donskoy',
+'Dragon Li', 'Foldex', 'Havana Brown', 'Highlander', 'Khao Manee',
+'Korat', 'Korn Ja', 'LaPerm', 'Minskin', 'Munchkin', 'Ojos Azules', 
+'Oregon Rex', 'PerFold', 'Persian (Modern)', 'Persian (Traditional)',
+'Peterbald', 'Raas', 'Ragamuffin', 'Sam Sawet', 'Serengeti', 
+'Serrade Petit', 'Singapura', 'Sokoke', 'Suphalak', 'Thai', 'Thai Lilac',
+'Toyger', 'Ukrainian Levkoy']
+
+# Add in some additional fun
+addcats = ['tiger', 'lion', 'ocelot', 'cheetah', 'leopard', 
+'panther', 'jaguar', 'snow leopard', 'cave lion', 'serval', 'caracal', 
+'lynx', 'bobcat', 'kitten', 'fishing cat', 'sand cat', 'ThunderCat']
+
+trimmedcats = [c for c in cats if c not in dropcats]
+fullcats = trimmedcats + addcats
+
 explevels = ['is an expert in', 'is mediocre at', 'studies', 'regularly makes use of', 'is a power-user of',
 'has an inordinate fondness for', 'flicks a tail at', 'has several degrees in', 'relaxes by thinking about',
 'can lecture at length on', 'could write a book on',
@@ -18,7 +39,7 @@ explevels = ['is an expert in', 'is mediocre at', 'studies', 'regularly makes us
 def catction():
     return "{0} the {1} {2} {3}.".format(
         re.sub("\d+ ","", random.choice(science.minor_planets["minor_planets"])),
-        random.choice(animals.cats["cats"]),
+        random.choice(fullcats),
         random.choice(explevels),
         random.choice(technology.computer_sciences['computer_sciences']))
 
